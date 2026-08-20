9a4045dad98c361d61c4494305d427caa1952e28
vpriya231-hub <vpriya231@gmail.com>
2026-08-20 22:26:31 +0530

    build: increment version code to 3

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
     applicationId = "com.flowtasks.app"
     minSdk = 24
     targetSdk = 36
-    versionCode = 2
+    versionCode = 3
     versionName = "1.0.0"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
